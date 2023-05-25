04ff7f9ed9fcdb42a65debd172ee8164c893e1d2
iloveyouamerica <iloveyouamerica@yandex.ru>
2023-05-25 15:10:59 +0300

    fix: убран ненужный слеш в utils/regex

--- utils/regex.js
+++ utils/regex.js
@@ -1,3 +1,3 @@
-const urlRegex = /^(http(s):\/\/.)[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)$/;
+const urlRegex = /^(http(s):\/\/.)[-a-zA-Z0-9@:%._+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_+.~#?&//=]*)$/;
 
 module.exports = { urlRegex };
